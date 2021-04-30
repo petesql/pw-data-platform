9854ba7fb39f78e2e06c5f781af58af425e13369
pete <peter_whyte@outlook.com>
2021-04-30 16:22:57 +0100

    chore: scope imports

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Cdk from '../lib/index';
+import { App } from '@aws-cdk/core';
+import { CdkStack } from '../lib/index';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
+    const app = new App();
     // WHEN
-    const stack = new Cdk.CdkStack(app, 'MyTestStack');
+    const stack = new CdkStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
