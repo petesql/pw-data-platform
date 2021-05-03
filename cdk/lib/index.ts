3244757c8cb0ce3f89f0dd2a0cc165dae1bc2b90
pete <peter_whyte@outlook.com>
2021-05-03 22:07:17 +0100

    feat: add default s3 bucket constructor
    also in here, for quickness... renaming the cdkstack to baseinfracdkstack.

--- cdk/lib/index.ts
+++ cdk/lib/index.ts
@@ -1,8 +1,9 @@
 import { Stack, StackProps, Construct } from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import { Ec2dbInstance } from './ec2-db-instance-stack';
+import { defaultS3Bucket } from './s3-bucket-stack';
 
-export class CdkStack extends Stack {
+export class BaseInfraCDKStack extends Stack {
     createVpc(scope: Construct) {
         return new ec2.Vpc(scope, 'VPC', {
             cidr: '10.0.0.0/24',
@@ -26,7 +27,7 @@ export class CdkStack extends Stack {
   }
 }
 
-export class Ec2dbStack extends CdkStack {
+export class Ec2dbStack extends BaseInfraCDKStack {
   constructor(scope: Construct, id: string, peerCidrIp: string, keyName: string, props?: StackProps) {
       super(scope, id, props);
       new Ec2dbInstance(this, 'ec2dbInstances',
@@ -34,5 +35,14 @@ export class Ec2dbStack extends CdkStack {
         peerCidrIp,
         this.availabilityZones[0],
         keyName);
+  }
+}
+
+export class S3Stack extends BaseInfraCDKStack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+      super(scope, id, props);
+
+      new defaultS3Bucket(this, 'bucketNamePete', 'bucketnameactualpete')
+
   }
 }
---
+++ cdk/lib/s3-bucket-stack.ts
@@ -0,0 +1,13 @@
+import * as core from "@aws-cdk/core";
+import * as s3 from '@aws-cdk/aws-s3';
+
+export class defaultS3Bucket extends core.Construct {
+    constructor(scope: core.Construct, id: string, bucketName: string) {
+        super(scope, id);
+
+        new s3.Bucket(this, 'pvtBucketCnst', {
+            bucketName: bucketName,
+        })
+
+    }
+}
